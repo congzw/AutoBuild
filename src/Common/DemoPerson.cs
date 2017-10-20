b6edede0adee8e77dc3c669e59df50ddb1474ec1
congzw <46074987@qq.com>
2017-10-20 11:10:35 +0800

    add demo

---
+++ src/Common/DemoPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Common
+{
+    public class DemoPerson
+    {
+        public string FirstName { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return "Hello from Common: " + FirstName;
+        }
+    }
+}
